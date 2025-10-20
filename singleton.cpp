651479785301937674f30b60f0706f87fa4de533
AnnaShunkar <anna.shunkar4@gmail.com>
2025-10-20 13:14:42 +0300

    Add files via upload

---
+++ singleton.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+//клас-одинак//
+class Logger {
+private:
+    static Logger* instance;
+    Logger() {} //приватний конструктор//
+
+public:
+    static Logger* getInstance() {
+        if (instance == nullptr) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    void log(const string& message) {
+        cout << "Log: " << message << endl;
+    }
+};
+
+// ініціалізація статичного члена
+Logger* Logger::instance = nullptr;
+
+int main() {
+    Logger* logger1 = Logger::getInstance();
+    Logger* logger2 = Logger::getInstance();
+
+    logger1->log("Hello from logger!");
+    logger2->log("This is the same instance.");
+
+    return 0;
+}
